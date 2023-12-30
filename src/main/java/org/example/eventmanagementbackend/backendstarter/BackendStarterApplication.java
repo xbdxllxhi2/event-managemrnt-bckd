73dfea4738d5e7c9d054f655af2ef38233a79243
xbdxllxhi2 <abdallahi2002.aa@gmail.com>
2023-12-30 01:09:09 +0100

    Feat: Feign client add and test

---
+++ src/main/java/org/example/eventmanagementbackend/backendstarter/BackendStarterApplication.java
@@ -0,0 +1,14 @@
+package org.example.eventmanagementbackend.backendstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BackendStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eventmanagementbackend/backendstarter/BackendStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventmanagementbackend.backendstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
